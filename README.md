eeebc88d1a2d12cd26900e27c9fcab5a8adbac1e
solanale <alejandro.solanas2@adidas.com>
2018-09-14 10:30:50 +0200

    RESTful API course example

--- README.md
+++ README.md
@@ -1,18 +1,24 @@
 # Node.js-MasterClass 
 
-> [Source](https://pirple.thinkific.com/courses/the-nodejs-master-class)
+> Build an MVC app, a REST API, and a CLI in plain Node JS (ES5 Javascript) with no NPM or 3rd-party libraries. [Source](https://pirple.thinkific.com/courses/the-nodejs-master-class)
 
-## Section 1: RESTful API
-
-## Section 2
-
-## Section 3
 ---
 
+## Section 1: RESTful API
+### Build a fully-functional JSON API.
+Parse request-paths, payloads, headers, query strings and route processing to handlers.
+
+## Section 2: Web App GUI
+### Build a presentation layer, frontend.
+Serve static assets from Node.js (CSS, images, and javascript files), and how to use Node to create complex templated views.
+
+## Section 3: CLI Application
+### Build a command-line application.
+Develop a system that takes inputs from the console, processes them, and writes outputs back out. Getting acquainted with the "readline", "events", "TTY", "V8", and "OS" modules in the process.
+
+---
 ## Assignment 1
-Hello World API
-> * RESTful JSON API that listen on a port of choice
-> * Returns JSON message on /hello 
+### Hello World API - [Readme](https://github.com/Naxsel/Node.js-MasterClass/tree/master/Assignment1/README.md)
 
-
-
+## Assignment 2
+###Pizza-delivery company API - [Readme](https://github.com/Naxsel/Node.js-MasterClass/tree/master/Assignment2/README.md)
--- RESTful API/index.js
+++ RESTful API/index.js
@@ -4,113 +4,26 @@
  */
 
 // Dependencies
-const http = require('http');
-const https = require('https')
-const url = require('url');
-const fs = require('fs')
-const StringDecoder = require('string_decoder').StringDecoder;
-const config = require('./config');
+var server = require('./lib/server');
+var workers = require('./lib/workers');
 
-// Instantiate the HTTP server
-var httpServer = http.createServer(function(req,res){
-  unifiedServer(req,res);
-});
+// Declare the app
+var app = {};
 
-httpServer.listen(config.httpPort,function(){
-  console.log('The server is up and running on port '+config.httpPort);
-});
+// Init function
+app.init = function(){
 
-// Instantiate the HTTPs server
-var httpsServerOptions = {
-  'key' : fs.readFileSync('./https/key.pem'),
-  'cert' : fs.readFileSync('./https/cert.pem')
-};
-var httpsServer = https.createServer(httpsServerOptions,function(req,res){
-  unifiedServer(req,res);
-});
+  // Start the server
+  server.init();
 
-httpsServer.listen(config.httpsPort,function(){
-  console.log('The server is up and running on port '+config.httpsPort);
-});
+  // Start the workers
+  workers.init();
 
-// All the server logic for both the http and https server
-var unifiedServer = function(req,res){
-  
-  // Parse the url
-  var parsedUrl = url.parse(req.url, true);
-
-  // Get the path
-  var path = parsedUrl.pathname;
-  var trimmedPath = path.replace(/^\/+|\/+$/g, '');
-
-  // Get the query string as an object
-  var queryStringObject = parsedUrl.query;
-
-  // Get the HTTP method
-  var method = req.method.toLowerCase();
-
-  //Get the headers as an object
-  var headers = req.headers;
-
-  // Get the payload,if any
-  var decoder = new StringDecoder('utf-8');
-  var buffer = '';
-  req.on('data', function(data) {
-      buffer += decoder.write(data);
-  });
-  req.on('end', function() {
-    buffer += decoder.end();
-
-    // Check the router for a matching path for a handler. If one is not found, use the notFound handler instead.
-    var chosenHandler = typeof(router[trimmedPath]) !== 'undefined' ? router[trimmedPath] : handlers.notFound;
-
-    // Construct the data object to send to the handler
-    var data = {
-      'trimmedPath' : trimmedPath,
-      'queryStringObject' : queryStringObject,
-      'method' : method,
-      'headers' : headers,
-      'payload' : buffer
-    };
-
-    // Route the request to the handler specified in the router
-    chosenHandler(data,function(statusCode,payload){
-
-      // Use the status code returned from the handler, or set the default status code to 200
-      statusCode = typeof(statusCode) == 'number' ? statusCode : 200;
-
-      // Use the payload returned from the handler, or set the default payload to an empty object
-      payload = typeof(payload) == 'object'? payload : {};
-
-      // Convert the payload to a string
-      var payloadString = JSON.stringify(payload);
-
-      // Return the response
-      res.setHeader('Content-Type', 'application/json');
-      res.writeHead(statusCode);
-      res.end(payloadString);
-      console.log("Returning this response: ",statusCode,payloadString);
-
-    });
-
-  });
 };
 
+// Self executing
+app.init();
 
-// Define all the handlers
-var handlers = {};
 
-// Ping handler
-handlers.ping = function(data,callback){
-  callback(200);
-};
-
-// Not found handler
-handlers.notFound = function(data,callback){
-  callback(404);
-};
-
-// Define the request router
-var router = {
-  'ping' : handlers.ping
-};
+// Export the app
+module.exports = app;
